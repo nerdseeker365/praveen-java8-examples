43adc2b272e8030f13bec12e1e52f846a5f3de1f
praveen <praveen@praveen-PC>
2019-10-24 07:33:13 +0530

    updated

--- src/main/java/com/praveen/java8/StreamExample.java
+++ src/main/java/com/praveen/java8/StreamExample.java
@@ -1,12 +1,18 @@
 package com.praveen.java8;
 
 import java.util.Arrays;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class StreamExample {
 	
 	public static void main(String[] args) {
-	System.out.println(Arrays.asList(1, 2, 3).stream() 			
-		.map(s -> String.valueOf(s).charAt(0)).findFirst());
+		
+		String sentence = "mango apple mango banana apple mango apple banana mango";
+		String[] words= sentence.split(" ");
+		System.out.println(Arrays.asList(words).stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting())));
+		
+		
 	}
 
 }
